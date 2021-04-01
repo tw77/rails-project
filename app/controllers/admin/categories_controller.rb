3e3cc855f369544b6d05572100d1a51a76c3d576
Thomas Weideman <thomas.weideman@gmail.com>
2021-04-01 17:44:14 +0000

    Generate admin/categories controller

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,22 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: Rails.configuration.admin_login[:name],
+                               password: Rails.configuration.admin_login[:password]
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(product_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+end
