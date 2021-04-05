13339164e98c35c6f7622150ca12a1a6aee84f4b
Thomas Weideman <thomas.weideman@gmail.com>
2021-04-05 21:00:06 +0000

    Add remaining product validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -14,5 +14,25 @@ RSpec.describe Product, type: :model do
       expect(@product).to_not be_valid
       expect(@product.errors.full_messages).to include("Name can't be blank")
     end
+
+    it 'requires a price' do
+      @category = Category.new(name: "test category")
+      @product = Product.new(name: "test product", price: nil, quantity: 1, category: @category)
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'requires a quantity' do
+      @category = Category.new(name: "test category")
+      @product = Product.new(name: "test product", price: 1, quantity: nil, category: @category)
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'requires a category' do
+      @product = Product.new(name: "test product", price: 1, quantity: 1, category: nil)
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
   end
 end
