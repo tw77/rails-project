d38477dcac2051c39a771d30bafbcd023bbe6f24
Thomas Weideman <thomas.weideman@gmail.com>
2021-04-06 00:16:15 +0000

    Define additional user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :email, :uniqueness => { :case_sensitive => false}, presence: true
+  validates :password, length: { minimum: 5 }
+  validates :password_digest, presence: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,25 +3,53 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validations' do
     it 'will save successfully with all fields set' do
-      @user = User.new(first_name: "One", last_name: "Two", email: "one@one.com", password: "one", password_confirmation: "one")
+      @user = User.new(first_name: "One", last_name: "Two", email: "one@one.com", password: "55555", password_confirmation: "55555")
       expect(@user).to be_valid
     end
 
     it 'requires a first name' do
-      @user = User.new(first_name: nil, last_name: "Two", email: "one@one.com", password: "one", password_confirmation: "one")
+      @user = User.new(first_name: nil, last_name: "Two", email: "one@one.com", password: "55555", password_confirmation: "55555")
       expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("First name can't be blank")
     end
 
     it 'requires a last name' do
-      @user = User.new(first_name: "One", last_name: nil, email: "one@one.com", password: "one", password_confirmation: "one")
+      @user = User.new(first_name: "One", last_name: nil, email: "one@one.com", password: "55555", password_confirmation: "55555")
       expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
     end
 
-  # requires an email
-  # requires a unique email
-  # requires a pw and pw confirmation
-  # both pw and pw confirmation fields are the same
-  # pw cannot be shorter than minimum length (to add to model and test)
+    it 'requires an email' do
+      @user = User.new(first_name: "One", last_name: "Two", email: nil, password: "55555", password_confirmation: "55555")
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
 
+    it 'requires a unique email' do
+      @user = User.new(first_name: "One", last_name: "Two", email: "one@one.com", password: "55555", password_confirmation: "55555")
+      @user.save!
+      @user1 = User.new(first_name: "Three", last_name: "Four", email: "one@one.com", password: "55555", password_confirmation: "55555")
+      expect(@user1).to_not be_valid
+      expect(@user1.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it 'requires a password and password confirmation' do
+      @user = User.new(first_name: "One", last_name: "Two", email: "one@one.com", password: nil, password_confirmation: nil)
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it 'requires password and password confirmation to be the same' do
+      @user = User.new(first_name: "One", last_name: "Two", email: "one@one.com", password: "55555", password_confirmation: "5555")
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'requires password to be five characters or longer' do
+      @user = User.new(first_name: "One", last_name: "Two", email: "one@one.com", password: "555", password_confirmation: "555")
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 5 characters)")
+    end
+      
   end
 end
